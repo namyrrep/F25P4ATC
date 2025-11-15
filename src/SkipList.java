e9f3efda034f56ee32f9482aa65a601668d097f7
edwinb1067 <edwinb1067@vt.edu>
2025-11-15 16:53:18 -0500

    Fixed all errors and warnings in SkipList

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,69 +1,52 @@
 import java.util.Random;
-import java.util.Dictionary;
 
-public class SkipList {
-    private SkipNode head;
+class SkipList<K extends Comparable<K>, E> {
+    private SkipNode<K, E> head;
     private int level;
     private int size;
-    static private Random ran = new Random(); // Hold the Random class
-                                              // object
+    private final Random ran = new Random(); // Hold the Random class object
 
     public SkipList() {
-        head = new SkipNode(null, null, 0);
+        head = new SkipNode<>(null, null, 0);
         level = -1;
         size = 0;
     }
 
-
-    // Return the (first) matching matching element if one exists, null
-    // otherwise
-    public Object find(Comparable<String> key) {
-        SkipNode x = head; // Dummy header node
-        for (int i = level; i >= 0; i--) { // For each level...
-            while ((x.forward[i] != null) && (x.forward[i].key().compareTo(
-                key) < 0)) { // go forward
+    // Return the (first) matching matching element if one exists, null otherwise
+    public E find(K key) {
+        SkipNode<K, E> x = head; // Dummy header node
+        for (int i = level; i >= 0; i--) // For each level...
+            while ((x.forward[i] != null) && (x.forward[i].key().compareTo(key) < 0)) // go forward
                 x = x.forward[i]; // Go one last step
-            }
-        }
         x = x.forward[0]; // Move to actual record, if it exists
-        if ((x != null) && (x.key().compareTo(key) == 0)) {
-            return x.element();
-        } // Got it
-        else {
-            return null;
-        } // Its not there
+        if ((x != null) && (x.key().compareTo(key) == 0)) return x.element(); // Got it
+        else return null; // Its not there
     }
 
-
     // Pick a level using a geometric distribution
-    int randomLevel() {
-        int lev;
-        for (lev = 0; Math.abs(ran.nextInt()) % 2 == 0; lev++) { // ran is
-                                                                 // random
-                                                                 // generator
-            ; // Do nothing
+    private int randomLevel() {
+        int lev = 0;
+        while(Math.abs(ran.nextInt()) % 2 == 0) {
+            lev++;
         }
         return lev;
     }
 
-
     /** Insert a key, element pair into the skip list */
-    public void insert(Comparable key, Object elem) {
+    @SuppressWarnings("unchecked")
+    public void insert(K key, E elem) {
         int newLevel = randomLevel(); // New node's level
-        if (newLevel > level) { // If new node is deeper
+        if (newLevel > level) // If new node is deeper
             adjustHead(newLevel); // adjust the header
-        }
         // Track end of level
-        SkipNode[] update = new SkipNode[level + 1];
-        SkipNode x = head; // Start at header node
+        SkipNode<K, E>[] update = new SkipNode[level + 1];
+        SkipNode<K, E> x = head; // Start at header node
         for (int i = level; i >= 0; i--) { // Find insert position
-            while ((x.forward[i] != null) && (x.forward[i].key().compareTo(
-                key) < 0)) {
+            while ((x.forward[i] != null) && (x.forward[i].key().compareTo(key) < 0))
                 x = x.forward[i];
-            }
             update[i] = x; // Track end at level i
         }
-        x = new SkipNode(key, elem, newLevel);
+        x = new SkipNode<>(key, elem, newLevel);
         for (int i = 0; i <= newLevel; i++) { // Splice into list
             x.forward[i] = update[i].forward[i]; // Who x points to
             update[i].forward[i] = x; // Who points to x
@@ -71,57 +54,58 @@ public class SkipList {
         size++; // Increment dictionary size
     }
 
-
     private void adjustHead(int newLevel) {
-        SkipNode temp = head;
-        head = new SkipNode(null, null, newLevel);
-        for (int i = 0; i <= level; i++) {
+        SkipNode<K, E> temp = head;
+        head = new SkipNode<>(null, null, newLevel);
+        for (int i = 0; i <= level; i++)
             head.forward[i] = temp.forward[i];
-        }
         level = newLevel;
     }
-    
-    private class KVPair {
-        private Comparable<String> key;
-        private Object value;
 
-        public KVPair(Comparable<String> k, Object v) {
+    class KVPair<K2 extends Comparable<K2>, E2> {
+        private final K2 key;
+        private final E2 value;
+
+        public KVPair(K2 k, E2 v) {
             key = k;
             value = v;
-        }
-        
-        public Comparable<String> key() {
+        }  
+
+        public K2 key() {
             return key;
         }
-        public Object value() {
+
+        public E2 value() {
             return value;
         }
-    }
         
+        @Override
+        public String toString() {
+            return "(" + key.toString() + "," + value.toString() + ")";
+        }
+    }
 
-    private class SkipNode {
-        private KVPair rec;
-        private SkipNode[] forward;
+    class SkipNode<K2 extends Comparable<K2>, E2> {
+        private final KVPair<K2, E2> rec;
+        private SkipNode<K2, E2>[] forward;
 
-        public Object element() {
+        public E2 element() {
             return rec.value();
         }
 
-
-        public Comparable<String> key() {
+        public K2 key() {
             return rec.key();
         }
 
-
-        public SkipNode(Comparable<String> key, Object elem, int level) {
-            rec = new KVPair(key, elem);
+        @SuppressWarnings("unchecked")
+        public SkipNode(K2 key, E2 elem, int level) {
+            rec = new KVPair<>(key, elem);
             forward = new SkipNode[level + 1];
-            for (int i = 0; i < level; i++) {
+            for (int i = 0; i < level; i++)
                 forward[i] = null;
-            }
         }
 
-
+        @Override
         public String toString() {
             return rec.toString();
         }
